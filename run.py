fbe489bf19b3f9da31b5904d74c61fd2355634bb
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-05-18 07:56:47 +0100

    Add requiements.txt file

--- run.py
+++ run.py
@@ -1,20 +1,50 @@
 import os
-from flask import Flask, render_template
+import json 
+from flask import Flask, render_template, request, flash
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title="home Gerson")
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    data = []
+    with open("data/company.json", 'r') as json_data:
+        data = json.load(json_data)
+    return render_template('about.html', page_title="about Gerson", list_of_numbers = [1,2,3,4,5], company=data)
 
-@app.route("/contact")
+
+
+@app.route("/about/<member_name>")
+def about_name(member_name):
+    member = {}
+    with open("data/company.json", 'r') as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member = obj
+    return render_template('detail.html', detail=member)
+
+
+@app.route("/contact",methods=["GET", "POST"] )
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        # print(request.form.get('name'))
+        # print(request.form['email'])
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
+    return render_template('contact.html', page_title="contact Gerson")
 
+
+@app.route("/careers")
+def careers():
+    return render_template('careers.html', page_title='careers gerson')
 
 if __name__ == "__main__":
     app.run(
